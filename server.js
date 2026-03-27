f76f3e00358a5b2dc18a6b425b362e61ad5032f1
devansh_283 <devanshprajapati99@gmail.com>
2026-03-27 13:15:31 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import apiRoutes from './routes/apiRoutes.js';
+import { sendNotificationEmail } from './controllers/mailController.js';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+// app.use(cors());
+app.use(cors({
+    origin: [   
+        'https://brand-incept-frontend.vercel.app',  // ← frontend deploy hone ke baad yahan lagao
+    ],
+    credentials: true,
+}));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Routes
+app.use('/api', apiRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Brand Incept API is running (No DB)');
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
